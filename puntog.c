ee056f0aad04c808210d22091788899a28e3c0e1
Tomas Gamarra <tomasgamarra2005@gmail.com>
2025-03-23 18:59:01 -0300

    Andando inciso g

--- puntog.c
+++ puntog.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+
 int controlEntradaYConversion(char *, int16_t *);
 int16_t digitHexToDec(char car);
 void imprimirBinario(int16_t num);
+void expresarDecimal(int16_t resul);
 
 int main(int argc, char const *argv[])
 {
@@ -17,7 +19,7 @@ int main(int argc, char const *argv[])
 
     // Tengo en resul la representacion de la entrada
     expresarDecimal(resul);
-    imprimirBinario(resul);
+    // imprimirBinario(resul);
     return 0;
 }
 
@@ -35,20 +37,40 @@ void expresarDecimal(int16_t resul)
     int16_t aux = 0;
     int16_t mascara = 0x8000;
     int16_t mascaraSigno;
-    if ((resul & mascara) > 0)
-        mascaraSigno = 0x8000;
-    else
+    if ((resul & mascara) == 0)
         mascaraSigno = 0;
+    else
+        mascaraSigno = 0x8000;
 
     // Ahora me quedo con la parte entera utilizando otra mascara que me deje los 7 bits despues del MSB
     mascara = 0x7F00;
     aux = mascara & resul;
     aux = aux >> 8;
     aux = aux | mascaraSigno;
+    if (mascaraSigno != 0)
+    {
+        int16_t mascaraNegativo = 0x7F80;
+        aux = aux | mascaraNegativo;
+    }
 
     printf("%d", aux); // Imprimo la parte entera
 
+    printf(".");
     // Me quedo con la parte fraccionaria utilizando otra mascara
+    mascara = 0x00FF;
+    aux = mascara & resul;
+
+    if (mascaraSigno != 0)
+    {
+        aux = ~(aux ^ 0xFF00);
+    }
+    int16_t nroImprimir = 0;
+    int16_t j = 10;
+    for (size_t i = 1; i < 5; i++, j = j * 10)
+    {
+        nroImprimir = (aux * j / 256) % 10;
+        printf("%d", nroImprimir);
+    }
 }
 
 int controlEntradaYConversion(char *num, int16_t *resul)
